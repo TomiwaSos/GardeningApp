cb8164a10373a9e6592260d3be9562258794834e
Tomiwa <you@example.com>
2023-11-05 19:13:48 +0000

    typo

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,10 @@
 class Client < ApplicationRecord
   validates :name, presence: true
+  validates :address, presence: true
+  validates :number, presence: true
   before_save { self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false },length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
-  validates :number, presence: true
-  validates :email, presence: true
 
   # has_many :invoices
   # has_many :jobs
