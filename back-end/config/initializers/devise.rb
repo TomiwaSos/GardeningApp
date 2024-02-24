7c9136f61661caaf18f651c542cb354f932cf00a
Tomiwa <you@example.com>
2024-02-24 22:47:49 +0000

    login is working as well as logout

--- back-end/config/initializers/devise.rb
+++ back-end/config/initializers/devise.rb
@@ -263,7 +263,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  config.navigational_formats = ['*/*', :html, :json]
+  config.navigational_formats = [:json]
 
 
   # The default HTTP method used to sign out a resource. Default is :delete.
@@ -311,4 +311,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  #
+  #
+  #
 end
