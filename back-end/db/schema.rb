30f5455925d50ca6cc8f263631b77cc45c16fabf
George Koundouri <georgekoundouri@msn.com>
2024-03-07 21:42:33 +0000

    Fixed invoice test issues

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_195119) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
